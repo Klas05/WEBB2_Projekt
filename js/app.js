ce14913ecdccaf584716ceeb19540cb8ddd6cd22
TSSReal <thesweslayer@gmail.com>
2024-05-31 16:48:38 +0200

    LKLK

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// const menu = document.querySelector("a.menu");
+
+// const slideElements = document.querySelectorAll(".slide");
+
+// slideElements.forEach((element) => {
+//   element.style.position = "relative";
+//   element.style.left = -2000 + "px";
+// });
+
+// menu.addEventListener("mouseover", function (e) {
+//   e.preventDefault();
+//   gsap.fromTo(".slide", { x: -2000 }, { x: 2000, duration: 3 });
+// });
