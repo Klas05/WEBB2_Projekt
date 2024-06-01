c412f1681899eb44b45a1e4288464b252b496bbe
TSSReal <thesweslayer@gmail.com>
2024-06-01 15:55:52 +0200

    Animation done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,54 @@
-// const menu = document.querySelector("a.menu");
+/**
+ * Hämtar nödvändiga element för att animera menyn
+ */
+const menu = document.querySelector("a.menu");
+const slideElements = document.querySelectorAll(".slide");
 
-// const slideElements = document.querySelectorAll(".slide");
+// Skapar en tidslinje för menyns animering
+var menuAnimation = gsap.timeline({ paused: true });
 
-// slideElements.forEach((element) => {
-//   element.style.position = "relative";
-//   element.style.left = -2000 + "px";
-// });
+// Variabel för att hålla koll på om menyn är öppen eller stängd
+let expanded = false;
 
-// menu.addEventListener("mouseover", function (e) {
-//   e.preventDefault();
-//   gsap.fromTo(".slide", { x: -2000 }, { x: 2000, duration: 3 });
-// });
+/**
+ * Denna funktion kallas när menyn ska öppnas. Först återställs tidslinjen så att inget återfinns inuti den. Sedan skapas animationen för menyn och spelas upp.
+ */
+function playMenuAnimation() {
+  menuAnimation.clear();
+
+  menuAnimation
+    .to("main", { y: 80, duration: 0.4, ease: "back(1)" })
+    .fromTo(
+      ".slide",
+      { y: 0, opacity: 0 },
+      { y: 80, opacity: 1, duration: 0.4, stagger: 0.08, ease: "back(1)" },
+      "<"
+    )
+    .play();
+}
+
+/**
+ * Denna funktion spelas när menyn ska stängas och spelar då upp tidslinjen baklänges.
+ */
+function reverseMenuAnimation() {
+  menuAnimation.reverse();
+}
+
+/**
+ * Lyssnar efter klick på meny knappen vilket sedan först förhindrar standard åtgärden för "a" taggar och sedan kollar om en meny animeringen redan spelas. Ifall den inte redan spelas så kontrolleras om menyn är öppen eller stängd, därefter spelas den korrekta animeringen, öppning eller stängning, och sedan uppdateras variabeln "expanded" för att korrespondera korrekt värde.
+ */
+menu.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (menuAnimation.isActive()) {
+    return;
+  }
+
+  if (!expanded) {
+    playMenuAnimation();
+  }
+  if (expanded) {
+    reverseMenuAnimation();
+  }
+  expanded = !expanded;
+});
